83713ee1937c4db0f5811ece04cb612aa2a9833c
dWX347607 <dWX347607@A150416457.china.huawei.com>
2016-11-29 20:22:11 +0800

    1129

--- src/app01/views.py
+++ src/app01/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+from .models import BBS, BBS_user
+
 # Create your views here.
 
 def index(request):
-    context = {}
+    bbs_list = BBS.objects.all()
+    context = {"bbs_list": bbs_list}
     return render(request, 'app01/index.html', context)
 
 def flappy(request):
