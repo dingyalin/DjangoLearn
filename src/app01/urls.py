ec193ba6bf5ef4899b2340f9f3f7f53c3c9bd18d
dWX347607 <dWX347607@A150416457.china.huawei.com>
2016-12-08 20:20:32 +0800

    20161208

--- src/app01/urls.py
+++ src/app01/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^detail/(\d+)$', views.detail, name='detail'),
     url(r'^flappy$', views.flappy, name='flappy'),
+    url(r'^sub_comment/$', views.sub_comment, name='sub_comment'),
 ]
--- src/app01/views.py
+++ src/app01/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.http import HttpResponseRedirect
+from django.template.context import RequestContext
 
-from .models import BBS, BBS_user
+from .models import BBS, BBS_user, Comments
 
 # Create your views here.
 
@@ -12,9 +14,14 @@ def index(request):
 
 def detail(request, bbs_id):
     bbs = BBS.objects.get(id=bbs_id)
-    context = {"bbs_obj": bbs}
+    comments = Comments.objects.filter(bbs=bbs_id)
+    context = {"bbs_obj": bbs, "comments": comments}
     return render(request, 'app01/detail.html', context)
 
 def flappy(request):
     context = {}
     return render(request, 'app01/flappy.html', context)
+
+def sub_comment(request):
+    print sub_comment.POST
+    return HttpResponseRedirect('/detail/1', context_instance=RequestContext(request))
